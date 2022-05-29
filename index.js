aabac319cc7e5d19592b1516ae7b99446ef9a6df
Full Name <oscaryiu.lapsang@gmail.com>
2022-05-29 13:21:28 +0800

    add logic

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 let billAmount = null;
 let tipPercentage = null;
 let numberOfPeople = null;
-let isCustomTipPercentage = false;
-
-tipPercentage = 0;
+let activeTipBtnId = null;
 
 function init() {
   document
@@ -12,39 +10,110 @@ function init() {
   document
     .getElementById("number-of-people")
     .addEventListener("input", handleNumberOfPeopleInput);
+  document
+    .getElementById("tip-percentage-5")
+    .addEventListener("click", handleClickTip);
+  document
+    .getElementById("tip-percentage-10")
+    .addEventListener("click", handleClickTip);
+  document
+    .getElementById("tip-percentage-15")
+    .addEventListener("click", handleClickTip);
+  document
+    .getElementById("tip-percentage-25")
+    .addEventListener("click", handleClickTip);
+  document
+    .getElementById("tip-percentage-50")
+    .addEventListener("click", handleClickTip);
+  document
+    .getElementById("reset-btn")
+    .addEventListener("click", handleClickReset);
+  document
+    .getElementById("tip-percentage-custom-btn")
+    .addEventListener("click", handleClickCustomTip);
 }
-function detectAndRenderError(
-  value,
-  borderElement,
-  warningElement,
-  inputElement
-) {
+
+function detectAndRenderError(value, warningElement, inputElement) {
   if (value == "") {
     inputElement.target.value = "";
-    borderElement.style.border = "#F4F8FA 3px solid";
     warningElement.style.display = "none";
+    inputElement.target.style.border = "";
     return true;
   } else if (value.includes("-")) {
     inputElement.target.value = value.replace("-", "");
-    borderElement.style.border = "#F4F8FA 3px solid";
     warningElement.style.display = "none";
+    inputElement.target.style.border = "";
     return true;
   } else if (Number(value) === 0) {
-    borderElement.style.border = "#ff2f00 3px solid";
+    inputElement.target.style.border = "#ff2f00 3px solid";
     warningElement.style.display = "block";
     warningElement.innerText = "Can't be zero";
     return true;
   } else {
-    borderElement.style.border = "#F4F8FA 3px solid";
     warningElement.style.display = "none";
+    inputElement.target.style.border = "";
     return false;
   }
 }
 
+function handleCustomTipInput(e) {
+  const warningElement = document.getElementById("tip-percentage-error");
+  detectAndRenderError(e.target.value, warningElement, e);
+  if (Number(e.target.value) === 0) {
+    tipPercentage = null;
+  } else {
+    tipPercentage = Number(e.target.value) / 100;
+  }
+  calculateResult();
+}
+
+function handleClickCustomTip() {
+  // unselect activeTipBtnId
+  handleUnselectTip();
+  document.getElementById("tip-percentage-custom-btn").style.display = "none";
+  document.getElementById("tip-percentage-custom-input").style.display =
+    "block";
+  document.getElementById("tip-percentage-custom-input").focus();
+  document
+    .getElementById("tip-percentage-custom-input")
+    .addEventListener("input", handleCustomTipInput);
+}
+
+function handleUnselectCustomTip() {
+  document
+    .getElementById("tip-percentage-custom-input")
+    .removeEventListener("input", handleCustomTipInput);
+  document.getElementById("tip-percentage-custom-btn").style.display = "block";
+  document.getElementById("tip-percentage-custom-input").style.display = "none";
+}
+
+function handleClickTip(e) {
+  handleUnselectCustomTip();
+  if (activeTipBtnId) {
+    document.getElementById(activeTipBtnId).className = "tip-btn";
+    if (activeTipBtnId === e.target.id) {
+      handleUnselectTip();
+      return;
+    }
+  }
+  e.target.className = "tip-btn-focus tip-btn";
+  activeTipBtnId = e.target.id;
+  tipPercentage = Number(e.target.id.split("-")[2]) / 100;
+  calculateResult();
+}
+
+function handleUnselectTip() {
+  if (activeTipBtnId) {
+    document.getElementById(activeTipBtnId).className = "tip-btn";
+    tipPercentage = null;
+    activeTipBtnId = null;
+    calculateResult();
+  }
+}
+
 function handleBillAmountInput(e) {
-  const borderElement = document.getElementById("bill-amount-input-container");
   const warningElement = document.getElementById("bill-amount-error");
-  detectAndRenderError(e.target.value, borderElement, warningElement, e);
+  detectAndRenderError(e.target.value, warningElement, e);
   if (Number(e.target.value) === 0) {
     billAmount = null;
   } else {
@@ -54,11 +123,8 @@ function handleBillAmountInput(e) {
 }
 
 function handleNumberOfPeopleInput(e) {
-  const borderElement = document.getElementById(
-    "number-of-people-input-container"
-  );
   const warningElement = document.getElementById("number-of-people-error");
-  detectAndRenderError(e.target.value, borderElement, warningElement, e);
+  detectAndRenderError(e.target.value, warningElement, e);
   if (Number(e.target.value) === 0) {
     numberOfPeople = null;
   } else {
@@ -70,9 +136,18 @@ function handleNumberOfPeopleInput(e) {
 function handleClickReset() {
   document.getElementById("bill-amount").value = "";
   document.getElementById("number-of-people").value = "";
-  document.getElementById("tip-percentage-custom").value = "";
+  if (document.getElementById("tip-percentage-custom")) {
+    document.getElementById("tip-percentage-custom").value = "";
+  }
+  if (activeTipBtnId) {
+    document.getElementById(activeTipBtnId).className = "tip-btn";
+  }
   document.getElementById("tip-amount-result").innerText = `$0.00`;
   document.getElementById("total-amount-result").innerText = `$0.00`;
+  handleUnselectCustomTip();
+  handleUnselectTip();
+  billAmount = null;
+  numberOfPeople = null;
 }
 
 function calculateResult() {
@@ -85,13 +160,6 @@ function calculateResult() {
     const totalAmount = billAmount + tipAmount;
     const tipAmountEachOne = (tipAmount / numberOfPeople).toFixed(2);
     const totalAmountEachOne = (totalAmount / numberOfPeople).toFixed(2);
-    console.log(
-      billAmount,
-      tipAmount,
-      totalAmount,
-      numberOfPeople,
-      totalAmountEachOne
-    );
     // confirm amount is more than zero
     if (Number(tipAmountEachOne) + Number(totalAmountEachOne) > 0) {
       document.getElementById(
